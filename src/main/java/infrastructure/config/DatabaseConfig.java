75dad9aec94e5e8518f3f520d505502f341db8b0
Diegofct <96diegofct@gmail.com>
2024-07-15 16:42:40 -0500

    fix: :bug: Se arregla un error en el archivo pom
    se arregla un error en el archivo pom

--- src/main/java/infrastructure/config/DatabaseConfig.java
+++ src/main/java/infrastructure/config/DatabaseConfig.java
@@ -5,11 +5,21 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConfig {
+
     private static final String URL = "jdbc:mysql://viaduct.proxy.rlwy.net:36702/railway";
     private static final String USER = "root";
     private static final String PASSWORD = "vcYNBvzOdHspdUoefmESInjgetrhslGr";
 
-    public static Connection getConnection() throws  SQLException {
+    // Bloque estático para cargar el controlador JDBC
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
 }
